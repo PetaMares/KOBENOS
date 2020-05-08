05e91b68a8ff094f13ed220c6d93637000356018
pemares <pemares@post.cz>
2020-05-08 18:29:14 +0000

    konkretizace kodu uzivatelu v configu a vytvoreni pdf reportu

--- src/pages/summaryPage.xaml.cs
+++ src/pages/summaryPage.xaml.cs
@@ -2,6 +2,11 @@
 using System.Windows.Controls;
 using kobenos.classes;
 
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
+
 namespace kobenos.pages
 {
     /// <summary>
@@ -9,6 +14,8 @@ namespace kobenos.pages
     /// </summary>
     public partial class SummaryPage : Page
     {
+        private ExecutionResult result;
+
         public SummaryPage()
         {
             InitializeComponent();
@@ -16,6 +23,7 @@ namespace kobenos.pages
 
         public void SetResult(ExecutionResult result)
         {
+            this.result = result;
             MainResult.DataContext = result;
         }
 
@@ -28,6 +36,29 @@ namespace kobenos.pages
         {
             MainWindow parentWindow = Window.GetWindow(this) as MainWindow;
             parentWindow.NavigateToExecutionPage();
+        }
+
+        private void GenerateReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Create a new PDF document
+            PdfDocument document = new PdfDocument();
+
+            //Add a page to the document
+            PdfPage page = document.Pages.Add();
+
+            //Create PDF graphics for the page
+            PdfGraphics graphics = page.Graphics;
+
+            //Set the standard font
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+
+            //Draw the text
+            graphics.DrawString(this.result.Name, font, PdfBrushes.Black, new PointF(0, 0));
+
+            FileStream fileStreamResult = new FileStream("pdf.pdf", FileMode.CreateNew);
+
+            document.Save(fileStreamResult);                     
+            fileStreamResult.Close();
         }
     }
 }
